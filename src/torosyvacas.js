94bb86b28987ac3a4ee8f016ddf1323738cff833
Mark04 <marco.escobar@ucb.edu.bo>
2022-04-23 15:29:36 -0400

    encuentraDosVacas

--- src/torosyvacas.js
+++ src/torosyvacas.js
@@ -3,10 +3,14 @@ class TorosYVacas{
         this.codigoSecreto = codigoSecreto;
       }
       adivinar(intento) {
-          if(intento == this.codigoSecreto)
-          return "Ganaste!";
-          else if(this.codigoSecreto.includes(intento[0]))
+        if(intento == this.codigoSecreto)
+        return "Ganaste!";
+        else if(this.codigoSecreto.includes(intento[0]) && 
+                this.codigoSecreto.includes(intento[1]))
+                return "**"
+            else if(this.codigoSecreto.includes(intento[0]))
             return "*"
+            
           return "";
       }
 }
