94735165edcccda66d65c8c5996e19e0dadbf13b
Mark04 <marco.escobar@ucb.edu.bo>
2022-04-25 21:37:13 -0400

    terminado

--- src/torosyvacas.js
+++ src/torosyvacas.js
@@ -4,18 +4,62 @@ class TorosYVacas{
         }
     adivinar(intento) {
         let respuesta = "";
-        /*if (intento === this.codigoSecreto) {
+        let posIni = 0;
+        if (intento === this.codigoSecreto) {
             return "Ganaste!";
-        }*/
-        for (var i = 0; i < this.codigoSecreto.length; i++) {
-            if(this.codigoSecreto[i] == intento[i] ){
-                respuesta += "!";
-            }
-            else if(this.codigoSecreto.includes(intento[i])) {
-            respuesta += "*";
-            }
         }
+        respuesta = this.buscarToros(intento,respuesta);
+        respuesta = this.buscarVacas(intento,respuesta);
+        respuesta = this.buscarTernera(intento,respuesta);
+    
         return respuesta;
+    }
+    buscarToros(intento,respuesta){                        
+        for (var i = 0; i < this.codigoSecreto.length; i++) {
+            if(this.codigoSecreto[i] == intento[i]){
+                respuesta += "!"                                
+            }                   
+        }                                 
+        return respuesta;
+    }
+    buscarVacas(intento,respuesta){        
+        for (var i = 0; i < this.codigoSecreto.length; i++) {
+            if( this.codigoSecreto.includes(intento[i]) &&
+                this.codigoSecreto[i] != intento[i]) {
+                respuesta = respuesta + "*"
+            } 
+        }
+        return respuesta;  
+    }
+    buscarTernera(intento,respuesta){                          
+        for (var i = 0; i < this.codigoSecreto.length; i++) {
+            if(this.codigoSecreto.includes(Number(intento[i]) + 1) || 
+                this.codigoSecreto.includes(Number(intento[i]) - 1)){
+                respuesta = respuesta + "#";
+            }                       
+                   
+        }
+        return respuesta;  
+    }    
+    getCodigoSecreto(posIni){
+        return  Number(this.codigoSecreto[posIni]);
+    }
+    esPrimo (valor){
+        let respuesta=true;
+        if(valor == 0 || valor == 1 || valor == 4){respuesta = false}
+        for(let i = 2;i<valor/2;i++){           
+            if(valor % i == 0){respuesta = false}
+        }
+        return respuesta
+    }
+    buscarBisonte(){
+        let respuesta = ""
+        for (var i = 0; i < this.codigoSecreto.length; i++) {
+            if(this.esPrimo(this.getCodigoSecreto(i))){
+                respuesta += "%"
+            } 
+        }
+        return respuesta
     }
 }
 export default TorosYVacas;
--- src/torosyvacas.test.js
+++ src/torosyvacas.test.js
@@ -1,9 +1,10 @@
 import TorosYVacas from "./torosyvacas";
+
 describe("Toros y Vacas -> ganar", () => {
-  /*it("Deberia responder 'Ganaste!' si se adivina el codigo secreto", () => {
+  it("Deberia responder 'Ganaste!' si se adivina el codigo secreto", () => {
     const torosyvacas = new TorosYVacas("3");
     expect(torosyvacas.adivinar("3")).toEqual("Ganaste!");
-  });*/
+  });
 
   it("Deberia responder '' si no  se adivina el codigo secreto", () => {
     const torosyvacas = new TorosYVacas("7");
@@ -12,28 +13,102 @@ describe("Toros y Vacas -> ganar", () => {
 });
 
 describe("Toros y Vacas -> Pista de vacas", () => {
-  it("Si ingresamos como codigo 12 y nuestro intento es 24 deberia retornar '*'", () => {
-    const torosyvacas = new TorosYVacas("12");
-    expect(torosyvacas.adivinar("24")).toEqual("*");
+  it("Si ingresamos como codigo 61 y nuestro intento es 15 deberia retornar '*#'", () => {
+    const torosyvacas = new TorosYVacas("61");
+    expect(torosyvacas.buscarVacas("15","")).toEqual("*");
   });
-  it("Si ingresamos como codigo 12 y nuestro intento es 21 deberia retornar '**'", () => {
-    const torosyvacas = new TorosYVacas("12");
-    expect(torosyvacas.adivinar("21")).toEqual("**");
+  it("Si ingresamos como codigo 45 y nuestro intento es 54 deberia retornar '**##'", () => {
+    const torosyvacas = new TorosYVacas("45");
+    expect(torosyvacas.buscarVacas("54","")).toEqual("**");
   });
-  it("Si ingresamos como codigo 124 y nuestro intento es 421 deberia retornar '*'", () => {
-    const torosyvacas = new TorosYVacas("124");
-    expect(torosyvacas.adivinar("421")).toEqual("***");
-  });
+  it("Si ingresamos como codigo 613 y nuestro intento es 361 deberia retornar '**##'", () => {
+    const torosyvacas = new TorosYVacas("613");
+    expect(torosyvacas.buscarVacas("361","")).toEqual("***");
+  });  
 });
 
 
 describe("Toros y Vacas -> Pista de toros", () => {
-  it("Si ingresamos como codigo 12 y nuestro intento es 14 deberia retornar '!'", () => {
+  it("Si ingresamos como codigo 32 y nuestro intento es 36 deberia retornar '!#'", () => {
+    const torosyvacas = new TorosYVacas("32");    
+    expect(torosyvacas.buscarToros("36","")).toEqual("!");
+  });  
+  it("Si ingresamos como codigo 513 y nuestro intento es 523 deberia retornar '!#'", () => {
+    const torosyvacas = new TorosYVacas("513");
+    expect(torosyvacas.buscarToros("523","")).toEqual("!!");
+  });  
+  it("Si ingresamos como codigo 6135 y nuestro intento es 2135 deberia retornar '!#'", () => {
+    const torosyvacas = new TorosYVacas("6135");
+    expect(torosyvacas.buscarToros("2135","")).toEqual("!!!");
+  });  
+});
+
+describe("Toros y Vacas -> Pista de terneras", () => {
+  it("Si ingresamos como codigo 12 y nuestro intento es 3 deberia retornar '#'", () => {
     const torosyvacas = new TorosYVacas("12");
-    expect(torosyvacas.adivinar("14")).toEqual("!");
+    expect(torosyvacas.buscarTernera("3","")).toEqual("#");
   });
-  it("Si ingresamos como codigo 45 y nuestro intento es 45 deberia retornar '**'", () => {
+
+  it("Si ingresamos como codigo 34 y nuestro intento es 5 deberia retornar '#'", () => {
+    const torosyvacas = new TorosYVacas("34");
+    expect(torosyvacas.buscarTernera("5","")).toEqual("#");
+  });
+
+  it("Si ingresamos como codigo 45 y nuestro intento es 36 deberia retornar '##'", () => {
     const torosyvacas = new TorosYVacas("45");
-    expect(torosyvacas.adivinar("45")).toEqual("!!");
+    expect(torosyvacas.buscarTernera("36","")).toEqual("##");
+  });
+  it("Si ingresamos como codigo 1234 y nuestro intento es 2346 deberia retornar '##'", () => {
+    const torosyvacas = new TorosYVacas("1234");
+    expect(torosyvacas.buscarTernera("2346","")).toEqual("###");
+  });
+});
+
+describe("Toros y Vacas -> Adivinar codigo", () => {
+  it("Si ingresamos como codigo 6123 y nuestro intento es 6213 deberia retornar '!!**###'", () => {
+    const torosyvacas = new TorosYVacas("6123");
+    expect(torosyvacas.adivinar("6213")).toEqual("!!**###");
+  });
+  it("Si ingresamos como codigo 5172 y nuestro intento es 7516 deberia retornar '***##'", () => {
+    const torosyvacas = new TorosYVacas("5172");  
+    expect(torosyvacas.adivinar("7516")).toEqual("***##");
+  });
+  it("Si ingresamos como codigo 613582 y nuestro intento es 412572 deberia retornar '***##'", () => {
+    const torosyvacas = new TorosYVacas("613582");  
+    expect(torosyvacas.adivinar("412572")).toEqual("!!!*######");
+  });
+});
+
+describe("Toros y Vacas -> Validar si es primo", () => {
+  it("Si ingresamos numero 5  deberia retornar 'true' ya que es un numero primo", () => {
+    const torosyvacas = new TorosYVacas("5");
+    expect(torosyvacas.esPrimo(torosyvacas.getCodigoSecreto(0))).toEqual(true);
+  });
+  it("Si ingresamos numero 4 deberia retornar 'false' ya que no es un numero primo", () => {
+    const torosyvacas = new TorosYVacas("4");
+    expect(torosyvacas.esPrimo(torosyvacas.getCodigoSecreto(0))).toEqual(false);
+  });
+  it("Si ingresamos numero 9 deberia retornar 'false' ya que no es un numero primo", () => {
+    const torosyvacas = new TorosYVacas("9");
+    expect(torosyvacas.esPrimo(torosyvacas.getCodigoSecreto(0))).toEqual(false);
+  });
+});
+
+describe("Toros y Vacas -> Pista de Bisonte", () => {
+  it("Si ingresamos como codigo 9  deberia retornar '%'", () => {
+    const torosyvacas = new TorosYVacas("5");
+    expect(torosyvacas.buscarBisonte()).toEqual("%");
+  });
+  it("Si ingresamos como codigo 9  deberia retornar ''", () => {
+    const torosyvacas = new TorosYVacas("9");
+    expect(torosyvacas.buscarBisonte()).toEqual("");
+  });
+  it("Si ingresamos como codigo 27  deberia retornar '%%'", () => {
+    const torosyvacas = new TorosYVacas("27");
+    expect(torosyvacas.buscarBisonte()).toEqual("%%");
+  });
+  it("Si ingresamos como codigo 235  deberia retornar '%%%'", () => {
+    const torosyvacas = new TorosYVacas("235");
+    expect(torosyvacas.buscarBisonte()).toEqual("%%%");
   });
 });
